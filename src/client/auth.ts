69f795c68249e5b57886e55dd129210f333824c8
bluewarn <n5lp97@gmail.com>
2022-02-12 00:38:05 +0900

    Refactor: Added request error message.

--- src/client/auth.ts
+++ src/client/auth.ts
@@ -1,14 +1,22 @@
+import type Swal from 'sweetalert2/dist/sweetalert2.js';
+import * as swal_setting from './swal_setting.js';
+
 const REDIRECT_URI = 'https://wtbc.bluewarn.dev';
 const CLIENT_ID = 'qrh8nkx7bzpij23zdudqsh05wzi9k0';
 const AUTH_SCOPE = 'chat:edit+chat:read+user:read:follows+user:read:subscriptions';
 
 class Auth {
-    static get_token(){
+
+    Toast = Swal.mixin(swal_setting.setting_def);
+    
+    get_token(){
         return fetch(`${REDIRECT_URI}/token`, {
             method: 'POST'
         }).then(res=>{
             history.replaceState({}, '', location.pathname);
             return res.json();
+        }).catch(err => {
+            this.onError();
         });
     }
 
@@ -16,20 +24,30 @@ class Auth {
      * 
      * @param cstate client state. For OAuth Authorization Code Flow CSRF prevention.
      */
-    static login(cstate: string){
+    login(cstate: string){
         location.replace(`${REDIRECT_URI}/login?cstate=${cstate}`);
     }
     
-    static logout(){
+    logout(){
         location.replace(`${REDIRECT_URI}/logout`);
     }
 
-    static token_refresh(){
+    token_refresh(){
         return fetch(`${REDIRECT_URI}/token/refresh`, {
             method: 'POST'
         }).then(res=>{
             if(!res.ok) return Promise.reject();
             return res.json();
+        }).catch(err => {
+            this.onError();
+        });
+    }
+
+    private onError(){
+        this.Toast.fire({
+            icon : 'error',
+            title : '서버 오류',
+            text : '알 수 없는 오류.'
         });
     }
 }
