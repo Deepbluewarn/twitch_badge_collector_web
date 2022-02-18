0cf91aad0c9c0f5e1e39a1cf9403174cda720418
bluewarn <n5lp97@gmail.com>
2022-02-18 19:55:43 +0900

    add i18n translations

---
+++ src/client/i18n/index.ts
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './locales/en';
+import ko from './locales/ko';
+
+const detectorOptions = {
+  order: ['localStorage', 'cookie', 'htmlTag', 'querystring'],
+  lookupLocalStorage: 'language',
+  lookupCookie: 'language',
+  htmlTag: document.documentElement,
+}
+i18n
+  .use(LanguageDetector)
+  .init({
+    resources: {
+        en: en,
+        ko: ko
+    },
+    // lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+    ns:['page', 'tmi'],
+    detection: detectorOptions
+  });
+
+  export default i18n
---
+++ src/client/i18n/locales/ko/index.ts
@@ -0,0 +1,6 @@
+import page from './page.json';
+import tmi from './tmi.json';
+
+export default{
+    page, tmi
+}
