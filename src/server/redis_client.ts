784d889271f0e2b06d8761f6b82478697aff8a72
bluewarn <n5lp97@gmail.com>
2022-03-04 22:28:01 +0900

    Separation of Redis server into session, cache server.

--- src/server/redis_client.ts
+++ src/server/redis_client.ts
@@ -1,17 +1,35 @@
 import logger from './utils/logger';
 const redis = require('ioredis');
 
+const sessClientOption = {
+    host: 'localhost',
+    port: 6379, 
+    password: process.env.SESSION_REDIS_AUTH,
+    db: 0
+}
+const cacheClientOption = {
+    host: 'localhost',
+    port: 6444, 
+    password: process.env.CACHE_REDIS_AUTH,
+    db: 0
+}
+const redisSessClient = redis.createClient(sessClientOption);
+const redisCacheClient = redis.createClient(cacheClientOption);
 
-export const redisClient = redis.createClient({
-    host:'localhost', 
-    port:6379, 
-    password:'*&o8BT%$437h$M'
+redisSessClient.on('connect',() => {
+    logger.info('connected to redis for session successfully!');
 });
 
-redisClient.on('connect',() => {
-    logger.info('connected to redis successfully!');
-})
-
-redisClient.on('error',(error) => {
-    logger.error('Redis connection error ', error);
+redisSessClient.on('error',(error) => {
+    logger.error('Redis for session connection error ', error);
 });
+
+redisCacheClient.on('connect',() => {
+    logger.info('connected to redis for cache successfully!');
+});
+
+redisCacheClient.on('error',(error) => {
+    logger.error('Redis for cache connection error ', error);
+});
+
+export {redisSessClient, redisCacheClient};
