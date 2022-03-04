f54e986c710914c1c8247a83ec3d2d0d9a51159d
bluewarn <n5lp97@gmail.com>
2022-03-04 22:25:16 +0900

    remove twitch api (move to proxy)

--- src/server/api/twitchApi.ts
+++ src/server/api/twitchApi.ts
@@ -1,81 +1,10 @@
 import axios, { AxiosRequestConfig, Method } from 'axios';
+import logger from '../utils/logger';
 
 class Twitch_Api {
     token: string;
 
-    get_users(user_token?: string, login?: string) {
-        let url = `https://api.twitch.tv/helix/users`;
-
-        if(login){
-            const params = new URLSearchParams();
-            params.append('login', login);
-            url = url + '?' + params;
-        }
-        return this.request(url, 'GET', null, user_token);
-    }
-    get_followed_streams(user_token: string, user_id: string, after?: string) {
-        console.log('get_followed_streams user_token : ', user_token);
-
-        if(after === null) return Promise.reject();
-
-        const params = new URLSearchParams();
-        params.append('user_id', user_id);
-        if(after) params.append('after', after);
-        params.append('first', '10');
-
-        const url = `https://api.twitch.tv/helix/streams/followed?${params}`;
-        return this.request(url, 'GET', null, user_token);
-    }
-
-    // server app access token 요청.
-
-    get_channel_chat_badges(broadcaster_id: string) {
-        const url = `https://api.twitch.tv/helix/chat/badges?broadcaster_id=${broadcaster_id}`;
-        return this.request(url, 'GET', null).then(badges => {
-            return badges;
-        });
-    }
-    get_global_chat_badges(/*map: boolean*/) {
-        const url = 'https://api.twitch.tv/helix/chat/badges/global';
-        return this.request(url, 'GET', null);
-    }
-    getChannelChatBadges(broadcaster_id: string){
-        const params = new URLSearchParams();
-        // params.append('language', i18n.language);
-        params.append('language', 'ko');
-        const url = `https://badges.twitch.tv/v1/badges/channels/${broadcaster_id}/display?${params}`
-        return this.request(url, 'GET', null).then(badges=>{
-            return badges;
-        });
-    }
-    getGlobalChatBadges(){
-        const params = new URLSearchParams();
-        // params.append('language', i18n.language);
-        params.append('language', 'ko');
-        const url = `https://badges.twitch.tv/v1/badges/global/display?${params}`
-        return this.request(url, 'GET', null).then(badges=>{
-            return badges;
-        });
-    }
-    get_emote_sets(emote_sets_id: string[]) {
-        const url = 'https://api.twitch.tv/helix/chat/emotes/set?';
-
-        const params = new URLSearchParams();
-        for(let i = 0; i < emote_sets_id.length; i++){
-            params.append('emote_set_id', emote_sets_id[i]);
-        }
-        return this.request(url + params, 'GET', null).then(sets=>{
-            return sets;
-        });
-    }
-    get_cheermotes(broadcaster_id: string){
-        const url = `https://api.twitch.tv/helix/bits/cheermotes?broadcaster_id=${broadcaster_id}`;
-        return this.request(url, 'GET', null).then(cm => {
-            return cm;
-        });
-    }
-
-    // Twitch Authrization
+    // Twitch Authorization
 
     async request_token(code: string) {
         const url = 'https://id.twitch.tv/oauth2/token?' + new URLSearchParams({
@@ -139,8 +68,9 @@ class Twitch_Api {
             headers: headers
         }
 
-        const res = (await axios(config)).data;
-        return res;
+        return axios(config).then(res => {
+            return res.data;
+        })
     }
 }
 
