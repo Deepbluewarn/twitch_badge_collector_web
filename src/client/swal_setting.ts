855715f9e0722338be8abf185917c662f18022e2
bluewarn <n5lp97@gmail.com>
2022-03-06 11:07:17 +0000

    add translation for filter setting alert

--- src/client/swal_setting.ts
+++ src/client/swal_setting.ts
@@ -1,5 +1,7 @@
 import Swal, { SweetAlertOptions } from 'sweetalert2';
 
+const theme = localStorage.getItem('theme');
+
 export const setting_def: SweetAlertOptions = {
     toast: true,
     position: 'top-end',
@@ -9,10 +11,23 @@ export const setting_def: SweetAlertOptions = {
     showCloseButton: true,
     timer: 6000,
     timerProgressBar: true,
+    color: 'var(--text-color-1)',
+    background: 'var(--bg-color-1)',
     didOpen: (toast) => {
         toast.addEventListener('mouseenter', Swal.stopTimer)
         toast.addEventListener('mouseleave', Swal.resumeTimer)
     }
+}
+export const ask_user: SweetAlertOptions = {
+    title: '',
+    text: "",
+    icon: 'warning',
+    showCancelButton: true,
+    color: 'var(--text-color-1)',
+    background: 'var(--bg-color-1)',
+    confirmButtonColor: 'var(--tbc-yellow)',
+    cancelButtonColor: 'var(--tbc-red)',
+    confirmButtonText: ''
 }
 
 export const user_info = {
