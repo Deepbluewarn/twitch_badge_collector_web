0a595adb83adac7463257d182cfdaa3a88fc3e22
bluewarn <n5lp97@gmail.com>
2022-04-07 21:05:10 +0900

    Add Sentry (Debugging tool)

---
+++ src/client/sentryInit.ts
@@ -0,0 +1,8 @@
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://a2bdedda18e64b42a825cdc515cff139@o1191182.ingest.sentry.io/6312438",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 0.25,
+});
