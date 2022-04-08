152f6686bcc9dee93d32447e06ad655e800431bc
bluewarn <n5lp97@gmail.com>
2022-04-09 01:19:32 +0900

    Change tracesSampleRate 0.25 to 1.0

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -4,5 +4,5 @@ import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
   dsn: "https://a2bdedda18e64b42a825cdc515cff139@o1191182.ingest.sentry.io/6312438",
   integrations: [new BrowserTracing()],
-  tracesSampleRate: 0.25,
+  tracesSampleRate: 1.0,
 });
