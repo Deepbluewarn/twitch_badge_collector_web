83200f91ef1b7ca811370764b112578ecc3868cf
bluewarn <n5lp97@gmail.com>
2022-04-10 04:25:24 +0900

    Update sentry.init

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -6,8 +6,7 @@ Sentry.init({
   dsn: "https://0361dbd0abb844de811e4bfc87cabc7b@o1197178.ingest.sentry.io/6319987",
   release: '4.4.4',
   integrations: [
-    new BrowserTracing(),
-    new RewriteFramesIntegration()
+    new BrowserTracing()
   ],
   tracesSampleRate: 1.0,
 });
