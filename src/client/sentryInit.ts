652ccf31bfd44c1120307e11eb126f1cc37fe34f
bluewarn <n5lp97@gmail.com>
2022-04-09 19:43:13 +0900

    update Sentry.init for rewrite abs_path

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -1,8 +1,14 @@
 import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
+import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations";
 
 Sentry.init({
   dsn: "https://a2bdedda18e64b42a825cdc515cff139@o1191182.ingest.sentry.io/6312438",
-  integrations: [new BrowserTracing()],
+  integrations: [
+    new BrowserTracing(),
+    new RewriteFramesIntegration({
+      prefix: ''
+    })
+  ],
   tracesSampleRate: 1.0,
 });
