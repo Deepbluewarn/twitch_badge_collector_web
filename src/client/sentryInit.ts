3225ddbb586dd99be8c7596fe4481da7e2e22fb3
bluewarn <n5lp97@gmail.com>
2022-04-10 04:04:07 +0900

    Update sentry dsn

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -3,7 +3,7 @@ import { BrowserTracing } from "@sentry/tracing";
 import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations";
 
 Sentry.init({
-  dsn: "https://a2bdedda18e64b42a825cdc515cff139@o1191182.ingest.sentry.io/6312438",
+  dsn: "https://0361dbd0abb844de811e4bfc87cabc7b@o1197178.ingest.sentry.io/6319987",
   integrations: [
     new BrowserTracing(),
     new RewriteFramesIntegration()
