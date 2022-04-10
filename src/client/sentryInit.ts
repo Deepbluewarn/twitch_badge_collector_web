dd7af16e6324c8e29cb837e1abdf59d0d5d5e9bc
bluewarn <n5lp97@gmail.com>
2022-04-10 15:59:06 +0900

    Update js for cdn

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -1,15 +1,10 @@
 import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
-import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations";
 
 Sentry.init({
   dsn: "https://fce2dd0e90344e28ad44a8edeae7d5e1@o1197563.ingest.sentry.io/6320351",
-  release: '4.4.4',
   integrations: [
     new BrowserTracing(),
-    // new RewriteFramesIntegration({
-    //   root: ''
-    // })
   ],
   tracesSampleRate: 1.0,
 });
