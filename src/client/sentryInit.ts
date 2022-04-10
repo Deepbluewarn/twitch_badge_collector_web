a717c8e55b083f4b75ca76d957a119156d66490e
bluewarn <n5lp97@gmail.com>
2022-04-10 15:21:11 +0900

    Update sentry.init

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -4,7 +4,7 @@ import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations"
 
 Sentry.init({
   dsn: "https://0361dbd0abb844de811e4bfc87cabc7b@o1197178.ingest.sentry.io/6319987",
-  release: '4.4.44',
+  release: '4.4.4',
   integrations: [
     new BrowserTracing(),
     // new RewriteFramesIntegration({
