10e7e7c690162f47a088ccecb8dd68a39fe1f078
bluewarn <n5lp97@gmail.com>
2022-04-10 13:23:27 +0900

    Add sourceMappingURL

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -6,7 +6,7 @@ Sentry.init({
   dsn: "https://0361dbd0abb844de811e4bfc87cabc7b@o1197178.ingest.sentry.io/6319987",
   release: '4.4.4',
   integrations: [
-    new BrowserTracing()
+    new BrowserTracing(),
   ],
   tracesSampleRate: 1.0,
 });
