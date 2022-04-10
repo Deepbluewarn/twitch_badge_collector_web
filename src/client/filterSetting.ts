59d7ff778668a02254a6c74ccbcb2c1aebca9f6f
bluewarn <n5lp97@gmail.com>
2022-04-10 15:53:47 +0900

    move sentry.init to filter

--- src/client/filterSetting.ts
+++ src/client/filterSetting.ts
@@ -7,6 +7,22 @@ import * as swal_setting from './swal_setting';
 
 import { BroadcastChannel } from 'broadcast-channel';
 
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
+import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations";
+
+Sentry.init({
+  dsn: "https://fce2dd0e90344e28ad44a8edeae7d5e1@o1197563.ingest.sentry.io/6320351",
+  release: '4.4.4',
+  integrations: [
+    new BrowserTracing(),
+    // new RewriteFramesIntegration({
+    //   root: ''
+    // })
+  ],
+  tracesSampleRate: 1.0,
+});
+
 const tapi: Twitch_Api = new Twitch_Api(CLIENT_ID);
 const Toast = Swal.mixin(swal_setting.setting_def);
 
