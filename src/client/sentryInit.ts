a4d2a61fdead72dd12f1cf7214596d67e2ad0708
bluewarn <n5lp97@gmail.com>
2022-04-10 17:41:13 +0900

    add rewriteframes

--- src/client/sentryInit.ts
+++ src/client/sentryInit.ts
@@ -1,10 +1,14 @@
 import * as Sentry from "@sentry/browser";
 import { BrowserTracing } from "@sentry/tracing";
+import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations";
 
 Sentry.init({
   dsn: "https://b2bb2ee45ae34702936b2e947fcecf0f@o1197585.ingest.sentry.io/6320366",
   integrations: [
     new BrowserTracing(),
+    new RewriteFramesIntegration({
+      root: '/src'
+    })
   ],
   release: '1.2.3',
   tracesSampleRate: 1.0,
